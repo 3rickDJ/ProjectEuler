625318f6a2945406118c7a8d5fe5985e04fa3cd9
3rick <erick.dejesus1@alumno.buap.mx>
2022-06-14 08:46:48 -0500

    typo: sintax error fixed

--- multiplesof3or5.py
+++ multiplesof3or5.py
@@ -2,7 +2,7 @@ from functools import reduce
 def main():
     numbers = []
     for number in range(1,1000):
-        if number%3 or number%5:
+        if number%3==0 or number%5==0:
             numbers.append(number)
 
     sumMultiplesOf3or5 = reduce(lambda number1, number2: number1 + number2, numbers)
