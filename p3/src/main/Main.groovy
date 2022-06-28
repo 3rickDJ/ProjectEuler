c18a77a18fbf171b1b27d50ed00fe57351b40888
3rick <erick.dejesus1@alumno.buap.mx>
2022-06-28 15:03:02 -0500

    update: 3rd PrimeNumbers

--- p3/src/main/Main.groovy
+++ p3/src/main/Main.groovy
@@ -1,10 +1,9 @@
 import static PrimeNumbers.*
 public class Main {
     public static void main(String[] args){
-        // def prime = [1,2,3,5,7,11,13,17,19]
-        // int num = 34580
-        print "Digit a number: "
-        long num = System.in.newReader().readLine() as long
+        long num = 34580L
+        // print "Digit a number: "
+        // long num = System.in.newReader().readLine() as long
         long maxFactor = maxFact(num)
         println "The hightest prime factor in $num is $maxFactor"
     }
--- p3/src/main/PrimeNumbers.groovy
+++ p3/src/main/PrimeNumbers.groovy
@@ -1,13 +1,26 @@
 class PrimeNumbers {
     static long maxFact(long  num){
-        long divisor=2
+        long divisor=2L
+        //Unique even prime '2'
+        while (!(num.equals(1L))){
+            long remainder = Long.remainderUnsigned(num,2L)
+            if(remainder.equals(0L)){
+                // println "$num : $divisor"
+                num=Long.divideUnsigned(num,divisor)
+            } else {
+                break
+            }
+        }
+        if(num.equals(1L)){return 2L}
+
+        //odd primes
+        divisor++
         while (!(num.equals(1L))){
             long remainder = Long.remainderUnsigned(num,divisor)
             if(remainder.equals(0L)){
-                // println "$num : $divisor"
                 num= Long.divideUnsigned(num,divisor)
             } else {
-                divisor++
+                divisor+=2
             }
         }
         divisor
