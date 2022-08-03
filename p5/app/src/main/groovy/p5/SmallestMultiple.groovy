27c81720aac99446396e2835c6c8eee35ed2cf0c
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-03 12:06:58 -0500

    refactor: conciser code
    Clean some commentaries

--- p5/app/src/main/groovy/p5/SmallestMultiple.groovy
+++ p5/app/src/main/groovy/p5/SmallestMultiple.groovy
@@ -1,11 +1,11 @@
 package p5
-import static PrimeList.*
+import static PrimeList.getPrimeList
 class SmallestMultiple {
     static def mCM(n){
         // def primeFactors = [[2,0],[3,0],[5,0],[7,0]] //getPrimeList
         if(n==1){return 1}
         def primeFactors = getPrimeList(n)
-        def maxExponents = primeFactors.collect{prime->
+        primeFactors.collect{prime->
             def counter = 0
             (2..n).each{
                 def counting = 0
@@ -15,16 +15,7 @@ class SmallestMultiple {
                 }
                 counter = counting > counter ? counting:counter
             }
-            counter
-        }//.inject(1){total,next -> total*next}
-
-        primeFactors.eachWithIndex{base, i->
-            primeFactors[i] = base**maxExponents[i]
-        }.inject(1){total, next -> total*next }
-        // def result = primeFactors.withIndex().collect{
-        //     base, i->
-        //     base**maxExponents[i]
-        //     }.join(1){total, next-> total*next}
-
+            prime**counter
+        }.inject(1){total,next -> total*next}
     }
 }
