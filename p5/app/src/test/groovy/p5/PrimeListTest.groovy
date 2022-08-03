eb1b8e9c037b99b2debe7cd59a67997853ea8ae7
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-03 11:48:12 -0500

    test: refactor methods' names
    Groovy allows declaring double quoted methods' names. However, we are
    using JUnit(GroovyTestCase), and it's necessary to name methods as Java's style.
    
    Also fixed typo. Single by double equal sign.

--- p5/app/src/test/groovy/p5/PrimeListTest.groovy
+++ p5/app/src/test/groovy/p5/PrimeListTest.groovy
@@ -1,19 +1,19 @@
 package p5
 import static PrimeList.*
 class PrimeListTest extends GroovyTestCase{
-    void "test Get Primes for range 1..5"{
+    void testGetPrimes5(){
         def list = getPrimeList(5)
-        assert list = [2,3,5]
+        assert list == [2,3,5]
     }
 
-    void "test Get Primes for range 1..10"{
+    void testGetPrimes10(){
         def list = getPrimeList(10)
-        assert list = [2,3,5,7]
+        assert list == [2,3,5,7]
     }
 
-    void "test Get Primes for raneg 1..20"{
+    void testGetPrimes20(){
         def list = getPrimeList(20)
-        assert list = [2,3,5,7,11,13,17,19]
+        assert list == [2,3,5,7,11,13,17,19]
     }
 
 }
--- p5/app/src/test/groovy/p5/SmallestMultipleTest.groovy
+++ p5/app/src/test/groovy/p5/SmallestMultipleTest.groovy
@@ -2,18 +2,18 @@ package p5
 
 class SmallestMultipleTest extends GroovyTestCase {
 
-    void "test1 number 5"(){
+    void testMCM5(){
         def result = SmallestMultiple.mCM(5)
-        assert result = 60
+        assert result == 60
     }
 
-    void "test2 number 8"(){
+    void testMCM8(){
         def result = SmallestMultiple.mCM(8)
-        assert result = 840
+        assert result == 840
     }
 
-    void "test3 number 10"(){
+    void testMCM10(){
         def result = SmallestMultiple.mCM(10)
-        assert result = 2520
+        assert result == 2520
     }
 }
