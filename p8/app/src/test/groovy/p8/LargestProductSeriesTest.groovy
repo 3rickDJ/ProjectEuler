e7e765370b91bbaa9ced7a224e6e9a8a7f6cf119
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-05 13:48:57 -0500

    test: add test for LargestProductSeries and its resource

---
+++ p8/app/src/test/groovy/p8/LargestProductSeriesTest.groovy
@@ -0,0 +1,22 @@
+package p8
+import static UtilReader.getDigits
+
+class LargestProductSeriesTest {
+    void testSeries2(){
+         def number = getDigits('1000-digitNumber.txt','src/test/resources/')
+         def result = LargestProductSeries.getLargestProduct(2, number)
+         assert result == [9,9]
+    }
+
+    void testSeries3(){
+         def number = getDigits('1000-digitNumber.txt','src/test/resources/')
+         def result = LargestProductSeries.getLargestProduct(3, number)
+         assert result == [9,9,8]
+    }
+
+    void testSeries4(){
+         def number = getDigits('1000-digitNumber.txt','src/test/resources/')
+         def result = LargestProductSeries.getLargestProduct(4, number)
+         assert result == [9,9,8,9]
+    }
+}
