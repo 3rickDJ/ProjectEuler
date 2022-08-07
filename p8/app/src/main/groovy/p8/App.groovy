a1f445a866a8d61f609fcc7677f9bb49a3476ecb
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-06 23:08:57 -0500

    refactor: app refactor simplier to read code

--- p8/app/src/main/groovy/p8/App.groovy
+++ p8/app/src/main/groovy/p8/App.groovy
@@ -1,9 +1,8 @@
 package p8
-import static LargestProductSeries.*
 import static UtilReader.getDigits
 
-def digitString = getDigits('1000-digitNumber.txt','src/main/resources/')
+def number = getDigits('1000-digitNumber.txt','src/main/resources/')
 
 def n = 13
-def result = getLargestProduct(n, digitString)
-println "The $n adjacent digits in the 1000-digit number that have the greatest product are $result"
+def result = LargestProductSeries.getLargestProduct(n, number)
+println "The $n adjacent digits in the 1000-digit number that have the greatest product are $result , ${result.inject(1){total, next -> total*next}}"
--- p8/app/src/main/groovy/p8/UtilReader.groovy
+++ p8/app/src/main/groovy/p8/UtilReader.groovy
@@ -1,6 +1,6 @@
 package p8
 class UtilReader {
-    static String getDigits(fileName, baseDir){
-        new File(baseDir, fileName).collect{it}
+    static String getDigits( fileName, baseDir ){
+        def reading = new File(baseDir, fileName).collect{it}.join('')
     }
 }
