9f5b31081683e7bcfd0050fc3e419558e10acdf1
3rick <erick.dejesus1@alumno.buap.mx>
2022-08-06 23:10:23 -0500

    refactor: largestProductSeries, concisier code and fix logical error
    There was a logical error, while trying to work with an ArrayList of
    bytes. This was intended to minimize the size of our program. However,
    as the length of adjacent numbers increases, the max size of a byte
    datum is insufficient.

--- p8/app/src/main/groovy/p8/LargestProductSeries.groovy
+++ p8/app/src/main/groovy/p8/LargestProductSeries.groovy
@@ -1,21 +1,25 @@
 package p8
 
 class LargestProductSeries {
-    static List getLP(len,num){
-        def numbers = num.collect { it -> it  as byte}
-        def max = [prod:1,index:0]
+    static List getLargestProduct(Integer len, String num){
+        List numbers = num.collect { it -> it  as BigInteger}
+        def max = [prod:0,index:0]
 
         def i=0
         while( i <= (numbers.size()-len ) ){
-            def slice = numbers[i..<(i+len)]
-            def zeroAt = slice.findIndexOf{it==0}
-            if( zeroAt == -1){
-                if(max.prod < slice.inject(1){total,next->total*next}){
-                    max.prod = slice.inject(1){total,next->total*next};
+            List slice = numbers[i..<(i+len)]
+            def product = slice.inject(1){total,next->total*next}
+
+            if(product==0){
+                i = slice.findIndexOf{it==0} + i +1
+            }else{
+                if(product > max.prod){
+                    max.prod = product
                     max.index = i
                 }
                 i = i + 1
-            } else { i = zeroAt + i + 1}
+            }
+
         }
         numbers[(max.index)..(max.index+len-1)]
     }
