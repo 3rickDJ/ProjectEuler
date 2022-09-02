dabfb89fd44af36fda041292758ff7da37999de3
3rick <erick.dejesus1@alumno.buap.mx>
2022-09-02 07:50:34 -0500

    test: fix OrdinalPrimeTest

--- p7/app/src/test/groovy/p7/OrdinalPrimeTest.groovy
+++ p7/app/src/test/groovy/p7/OrdinalPrimeTest.groovy
@@ -1,7 +1,7 @@
 package p7
 
 import static OrdinalPrime
-class OrdinalPrimeTest {
+class OrdinalPrimeTest extends GroovyTestCase{
     void testOrdinal4(){
         def result = ordinal(3)
         assert result == 5
